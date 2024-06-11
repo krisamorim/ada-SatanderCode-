faf78f900543423c531076c6b1344a12da93a7b1
krisamorim <kris.a.furtado@gmail.com>
2024-06-10 21:51:07 -0300

    1st chalenge

---
+++ Python/Assessments001/chaleng.py
@@ -0,0 +1,9 @@
+#improved solution
+list_B = [10, 20, 30, 40, 50]
+print(f'Original list: {list_B}\nInverted list: {list_B[::-1]}')
+
+#Initial solution
+# list_A = [1, 2, 3, 4, 5]
+# def inverted_list(list_X):
+#     return list_X[::-1]
+# print(inverted_list(list_A))
